74c30d6905329b9d35f400db9677609bf76aecf3
Segun <miksegun1996@gmail.com>
2024-10-07 21:10:25 +0100

    all done

--- lib/main.dart
+++ lib/main.dart
@@ -3,17 +3,23 @@ import 'package:blott_mobile_app/src/data/repository/news_repository.dart';
 import 'package:blott_mobile_app/src/screens/news_screen.dart';
 import 'package:blott_mobile_app/src/screens/signup_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
 import 'src/binding/dependencies/dependencies.dart' as dep;
 
 Future<void> main() async {
-  runApp(const MyApp());
   await dep.init();
   await GetStorage.init();
   WidgetsBinding.instance.addPostFrameCallback((_) async{
     await Get.find<GeneralNewsController>().getUserData();
   });
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+
+  await Future.delayed(const Duration(seconds: 5));
+  runApp(const MyApp());
+  FlutterNativeSplash.remove();
 }
 
 class MyApp extends StatelessWidget {
